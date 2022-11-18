a9e62fa53e3766c34c377827cfbdfb0f2b3019c1
Krzysztof Witek <kwitek@webbit.pl>
2022-11-18 02:41:02 +0100

    Add empty messages view

--- src/views/MessageView.vue
+++ src/views/MessageView.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
+  <div class="page_content">
+    <div class="card full_height">
     <!-- message view content -->
-    
+      <div class="header">
+        <div class="title">
+          Wiadomości
+        </div>
+      </div>
+
+      <div class="body">
+        <!-- message related components -->
+
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+
+</script>
